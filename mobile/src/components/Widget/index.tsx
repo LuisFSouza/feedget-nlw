08b1aa2b522536052b0e4ea9133a9c911463e63a
Luis Felipi Souza <330luisfelipe@gmail.com>
2022-05-05 12:07:00 -0300

    MOBILE - criando o Widget

---
+++ mobile/src/components/Widget/index.tsx
@@ -0,0 +1,44 @@
+import { ChatTeardropDots } from 'phosphor-react-native';
+import React, { useRef } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { theme } from '../../theme';
+import BottomSheet from '@gorhom/bottom-sheet';
+import { gestureHandlerRootHOC } from 'react-native-gesture-handler';
+
+import { styles } from './styles';
+
+function Widget() {
+
+    const bottomSheetRef = useRef<BottomSheet>(null)
+
+    function handleOpen() {
+        bottomSheetRef.current?.expand()
+    }
+
+    return (
+        <>
+            <TouchableOpacity
+                style={styles.button}
+                onPress={handleOpen}
+            >
+                <ChatTeardropDots
+                    size={24}
+                    weight="bold"
+                    color={theme.colors.text_on_brand_color}
+
+                />
+            </TouchableOpacity>
+
+            <BottomSheet
+                ref={bottomSheetRef}
+                snapPoints={[1, 280]}
+                backgroundStyle={styles.modal}
+                handleIndicatorStyle={styles.indicator}
+            >
+
+            </BottomSheet>
+        </>
+    );
+}
+
+export default gestureHandlerRootHOC(Widget)
